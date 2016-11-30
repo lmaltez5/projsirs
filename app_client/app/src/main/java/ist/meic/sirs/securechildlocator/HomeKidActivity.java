e1756bc6e419b4110706f8a3b5a8f8ffc1066abe
OliveiraRuben <rubenoliveira94@gmail.com>
2016-11-30 23:00:44 +0000

    Add kids home page

---
+++ app_client/app/src/main/java/ist/meic/sirs/securechildlocator/HomeKidActivity.java
@@ -0,0 +1,13 @@
+package ist.meic.sirs.securechildlocator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeKidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_kid);
+    }
+}
