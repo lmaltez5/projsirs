ee4e76c2596b7f3e336f81f8cb2a5fcbe11c523e
guilherme93 <guilherme_753@msn.com>
2016-12-04 02:09:40 +0000

    sessionkey implemented

--- server/maven/src/main/java/org/child/secure/locator/maven/ClientHandlerThread.java
+++ server/maven/src/main/java/org/child/secure/locator/maven/ClientHandlerThread.java
@@ -5,6 +5,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -37,7 +40,8 @@ public class ClientHandlerThread extends Thread{
 	{
 	    try
 	    {
-	        String fromClient,username,email,password,timeStamp,phoneID,phoneName;
+	        String fromClient,username,email,password,timeStamp,phoneID,phoneName,clientKey;
+	        boolean temp;
 	        dbconnector = new DBConnector();
 			dbconnector.connect();
 	        while (running && (fromClient = reader.readLine()) != null)
@@ -59,56 +63,81 @@ public class ClientHandlerThread extends Thread{
 					     password = tokens[3];
 					     timeStamp=tokens[4];
 					     if(verifyDate(timeStamp)){
-					    	 verifyDbResult(dbconnector.insertSignup(username,email,password),"Signup");
-					     }
+					    	 temp=dbconnector.insertSignup(username,email,password);
+					    	 verifyDbResult(temp,"Signup");
+					    	 if (temp){
+						    		String serverKey=generateRandomSecret();
+						    		writer.println(serverKey);
+						 			writer.flush();
+						    		/*INSERT IN BD
+						    		dbconnector.uniqueEmail(email);
+						 			writer.flush();*/
+						 		}
+					    }
 			         break;
 					 case 2:
 					     email=tokens[1];
 					     password = tokens[2];
 					     timeStamp=tokens[3];
 					     if(verifyDate(timeStamp)){
-					    	 verifyDbResult(dbconnector.login(email,password),"Login");
+					    	 temp=dbconnector.login(email,password);
+					    	 verifyDbResult(temp,"Login");
+					    	 if (temp){
+						    		String serverKey=generateRandomSecret();
+						    		writer.println(serverKey);
+						 			writer.flush();
+						    		/*INSERT IN BD
+						    		dbconnector.uniqueEmail(email);
+						 			writer.flush();*/
+						 		}
 					     }
 			         break;
 					 case 3:
-						 phoneID=tokens[1];
-						 email=tokens[2];
-						 phoneName=tokens[3];
-						 timeStamp=tokens[4];
-						 if(verifyDate(timeStamp)){
-							 verifyDbResult(dbconnector.insertIDChild(email, phoneID , phoneName),"Child ID");
+						 clientKey=tokens[1];
+						 phoneID=tokens[2];
+						 email=tokens[3];
+						 phoneName=tokens[4];
+						 timeStamp=tokens[5];
+						 if(verifyDate(timeStamp)&& dbconnector.verifyKey(clientKey,email,phoneID)){
+							 verifyDbResult(dbconnector.insertID(email, phoneID , phoneName,"child"),"Child ID");
 					     }
 					 break;
 					 case 4:
-						 phoneID=tokens[1];
-						 email=tokens[2];
-						 phoneName=tokens[3];
-						 timeStamp=tokens[4];
-						 if(verifyDate(timeStamp)){
-							 verifyDbResult(dbconnector.insertIDParent(email, phoneID , phoneName),"Parent ID");
+						 clientKey=tokens[1];
+						 phoneID=tokens[2];
+						 email=tokens[3];
+						 phoneName=tokens[4];
+						 timeStamp=tokens[5];
+						 if(verifyDate(timeStamp)&& dbconnector.verifyKey(clientKey,email,phoneID)){
+							 verifyDbResult(dbconnector.insertID(email, phoneID , phoneName,"parent"),"Parent ID");
 					     }
 					 break;
 					 case 5:
-						 phoneID=tokens[1];
-						 email=tokens[2];
-						 timeStamp=tokens[3];
-						 if(verifyDate(timeStamp)){
+						 clientKey=tokens[1];
+						 phoneID=tokens[2];
+						 email=tokens[3];
+						 timeStamp=tokens[4];
+						 if(verifyDate(timeStamp)&& dbconnector.verifyKey(clientKey,email,phoneID)){
 							 writer.println(dbconnector.queryPhoneId(email, phoneID));
 							 writer.flush();
 					     }
 					 break;
 					case 6:
-						 email=tokens[1];
-						 timeStamp=tokens[2];
-						 if(verifyDate(timeStamp)){
+						 clientKey=tokens[1];
+						 phoneID=tokens[2];
+						 email=tokens[3];
+						 timeStamp=tokens[4];
+						 if(verifyDate(timeStamp)&& dbconnector.verifyKey(clientKey,email,phoneID)){
 							 writer.println(dbconnector.searchUser(email));
 							 writer.flush();
 					     }
 					 break;
 					case 7:
-						 email=tokens[1];
-						 timeStamp=tokens[2];
-						 if(verifyDate(timeStamp)){
+						 clientKey=tokens[1];
+						 phoneID=tokens[2];
+						 email=tokens[3];
+						 timeStamp=tokens[4];
+						 if(verifyDate(timeStamp)&& dbconnector.verifyKey(clientKey,email,phoneID)){
 							writer.println(dbconnector.searchPhoneNames(email));
 							writer.flush();
 					     }
@@ -143,15 +172,29 @@ public class ClientHandlerThread extends Thread{
 	public void setRunning(boolean running){
 	    this.running = running;
 	}
+	private String generateRandomSecret(){
+		try {
+			 SecureRandom random = new SecureRandom();
+			 byte seed[] = random.generateSeed(256);
+			 MessageDigest md=MessageDigest.getInstance("SHA-256");
+			 md.update(seed);
+	         byte[] digest = md.digest();
+	         return digest.toString().replace("\n", "");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			System.exit(1);
+			return null;
+		}
+        
+	}
+	
 	private boolean verifyDate(String date1){
 		try {
 			Calendar c = Calendar.getInstance();  
 	        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");  
-	        String formattedDate = df.format(c.getTime());
-	        Date sDt;
-	        Date sDt2;
-			sDt = df.parse(date1);
-			sDt2 = df.parse(formattedDate);
+	        String formattedDate = df.format(c.getTime());;
+	        Date sDt = df.parse(date1);
+			Date sDt2 = df.parse(formattedDate);
 			long ld = sDt.getTime() /1000;  
 	        long ld2 = sDt2.getTime() /1000;
 	        if(ld2>ld){
