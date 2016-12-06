708afb385c4694946737691ddf8bb1b41e1bb521
guilherme93 <guilherme_753@msn.com>
2016-12-06 16:48:22 +0000

    changes

--- server/maven/src/main/java/org/child/secure/locator/maven/sslServer.java
+++ server/maven/src/main/java/org/child/secure/locator/maven/sslServer.java
@@ -20,34 +20,11 @@ public class sslServer extends Thread
 	
 	    try
 	    {
-	    	 Security.addProvider(new Provider());
-			//Specifying the Keystore details
-			 
-			System.setProperty("javax.net.ssl.keyStore","serverKey.keystore");
-			System.setProperty("javax.net.ssl.keyStorePassword","EpW5eE[bxwHu");
-			
-			sslFac=(SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-	       /* System.out.print("Validating SSL certificate... ");
-	        KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
-	        keyStore.load(new FileInputStream(certificateDir), password);
-	        System.out.println("DONE.");
-	
-	        System.out.print("Creating trust engine........ ");
-	        TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-	        tmf.init(keyStore);
-	        System.out.println("DONE.");
-	
-	        System.out.print("Creating key engine.......... ");
-	        KeyManagerFactory kmf = KeyManagerFactory.getInstance((KeyManagerFactory.getDefaultAlgorithm()));
-	        kmf.init(keyStore, password);
-	        System.out.println("DONE.");
-	
-	        System.out.print("Creating SSL context......... ");
-	        System.setProperty("https.protocols", "SSL");
-	        SSLContext  ctx = SSLContext.getInstance("SSL");
-	        ctx.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-	        sslFac = ctx.getServerSocketFactory();
-	        System.out.println("DONE.");*/
+	    	 Security.addProvider(new Provider());	 
+		 System.setProperty("javax.net.ssl.keyStore","serverKey.keystore");
+		 System.setProperty("javax.net.ssl.keyStorePassword","EpW5eE[bxwHu");
+		 sslFac=(SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+	      
 	    }
 	    catch (Exception e) {
 	    	System.out.println(e.toString() + " ::: " + e.getCause());
@@ -66,16 +43,16 @@ public class sslServer extends Thread
 	        System.exit(-1);
 	    }
 	    while (running) {
-				try {
-					SSLSocket sslDataTrafficSocketInstance = (SSLSocket) sslDataTraffic.accept();
-					sslDataTrafficSocketInstance.setEnabledCipherSuites(new String[] {"TLS_DHE_RSA_WITH_AES_128_CBC_SHA256"});
-					ClientHandlerThread c = new ClientHandlerThread(sslDataTrafficSocketInstance);
-		            c.start();
-		            connectedClients.add(c);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+		try {
+			SSLSocket sslDataTrafficSocketInstance = (SSLSocket) sslDataTraffic.accept();
+			sslDataTrafficSocketInstance.setEnabledCipherSuites(new String[] {"TLS_DHE_RSA_WITH_AES_128_CBC_SHA256"});
+			ClientHandlerThread c = new ClientHandlerThread(sslDataTrafficSocketInstance,this);
+			c.start();
+			connectedClients.add(c);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	            
-	        }
+          }
    }
 }
