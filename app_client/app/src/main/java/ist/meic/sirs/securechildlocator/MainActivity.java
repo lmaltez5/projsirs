1bd301653f88b370d9a0a99622e0a273c5ea90c6
lmaltez5 <lmaltez5@gmail.com>
2016-12-08 22:23:54 +0000

    sdk 23

--- app_client/app/src/main/java/ist/meic/sirs/securechildlocator/MainActivity.java
+++ app_client/app/src/main/java/ist/meic/sirs/securechildlocator/MainActivity.java
@@ -33,8 +33,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if ( Build.VERSION.SDK_INT >= 23 &&
-                ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {}
+          int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {android.Manifest.permission.INTERNET, android.Manifest.permission.READ_PHONE_STATE,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION};
+ 
+    if(!hasPermissions(this, PERMISSIONS)){
+        ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+    }
+ 
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -65,6 +71,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+       public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+         for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+               }
+        }
+    }
+        return true;
+    }
+    
     public void login(){
         Log.d(TAG, "Login");
 
